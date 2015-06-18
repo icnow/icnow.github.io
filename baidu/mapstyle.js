3e43b452464c23059b72327863a2590b75ff2bd3
tianyi <icnow@icnows-MacBook-Pro.local>
2015-06-18 10:28:47 +0800

    baidu

---
+++ baidu/mapstyle.js
@@ -0,0 +1,126 @@
+/**
+ * Created by lzxue on 2015/2/15.
+ */
+var style=[
+    {
+        "featureType": "land",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#212121"
+        }
+    },
+    {
+        "featureType": "building",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#2b2b2b"
+        }
+    },
+    {
+        "featureType": "highway",
+        "elementType": "all",
+        "stylers": {
+            "color": "#ffffff",
+            "visibility": "off"
+        }
+    },
+    {
+        "featureType": "arterial",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#ffffff",
+            "lightness": -77,
+            "saturation": -94,
+            "visibility": "on"
+        }
+    },
+    {
+        "featureType": "green",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#1b1b1b"
+        }
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#181818"
+        }
+    },
+    {
+        "featureType": "subway",
+        "elementType": "geometry.stroke",
+        "stylers": {
+            "color": "#181818"
+        }
+    },
+    {
+        "featureType": "railway",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#ffffff",
+            "lightness": -52
+        }
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": {
+            "color": "#313131"
+        }
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": {
+            "color": "#8b8787"
+        }
+    },
+    {
+        "featureType": "manmade",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#1b1b1b"
+        }
+    },
+    {
+        "featureType": "local",
+        "elementType": "geometry",
+        "stylers": {
+            "lightness": -75,
+            "saturation": -91,
+            "visibility": "off"
+        }
+    },
+    {
+        "featureType": "subway",
+        "elementType": "geometry",
+        "stylers": {
+            "lightness": -65
+        }
+    },
+    {
+        "featureType": "railway",
+        "elementType": "all",
+        "stylers": {
+            "lightness": -40
+        }
+    },
+    {
+        "featureType": "boundary",
+        "elementType": "geometry",
+        "stylers": {
+            "color": "#ffffff",
+            "weight": "1"
+        }
+    },
+    {
+        "featureType": "label",
+        "elementType": "labels.text.fill",
+        "stylers": {
+            "color": "#ffffff",
+            "weight": "8"
+        }
+    }
+]
